83ffd7985672a3973b85b30c000c76f2a381c1d9
Arcticfaded <jbelt021@fiu.edu>
2016-07-07 19:01:47 -0400

    bug: fixing branch

--- git_pull.bash
+++ git_pull.bash
@@ -1,10 +1,4 @@
 while sleep 1
 do
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse @{u})
-BASE=$(git merge-base @ @{u})
-
-if [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
-fi  
+git pull origin master 
 done
