9261a81152c307b8ab6fe3e97360bc232e69c858
Arcticfaded <jbelt021@fiu.edu>
2016-07-07 19:24:52 -0400

    feat: automatically pulls the remote repository

--- git_pull.bash
+++ git_pull.bash
@@ -1,4 +1,10 @@
+OBJECT="$(git ls-remote origin -h refs/heads/master)"
 while sleep 1
 do
-git pull origin master 
+CURRENT="$(git ls-remote origin -h refs/heads/master)"
+if [ "$OBJECT" != "$CURRENT" ];
+then
+	git pull origin master
+	OBJECT=$CURRENT	
+fi
 done
