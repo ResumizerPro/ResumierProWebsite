5cecbf7502ba26770038e310003212cf4620265e
JonathanBeltran <jbelt021@fiu.edu>
2016-07-08 13:50:31 -0400

    feat: adding filtering

--- app/filter.js
+++ app/filter.js
@@ -1,9 +1,10 @@
 angular.module("myApp.filters", []).filter("titlesize", function(){
   return function(input) {
-    var str = String(input).split();
-    str.forEach(function(a){
-      a = a[0].toUpperCase() + a.splice(1).toLowerCase;
-    });
-    return str;
+    var str = String(input).split(' ');
+    str = str.map(function(a){
+      a = a[0].toUpperCase() + a.slice(1).toLowerCase();
+      return a;
+    }).join(' ');
+	return str;
   }
 });
