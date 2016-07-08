4d1244f5f4f26fb49e4a2b1fcc0ced9146ce9f20
JonathanBeltran <jbelt021@fiu.edu>
2016-07-07 21:31:42 -0400

    feat: remove static content

--- app/controllers.js
+++ app/controllers.js
@@ -1,15 +1,6 @@
 angular.module("myApp.controllers", []).controller("resumeCtrl", function($scope) {
   // Write your code here
-  $scope.resumes = [{
-  artist: "Nightwish",
-  title: "Ghost Loves Score"
-}, {
-  artist: "Evanescence",
-  title: "Everybody's Fool"
-}, {
-  artist: "Within Temptation",
-  title: "Ice Queen"
-}];
+  $scope.resumes = [];
 
 $scope.newResumes = { };
 
