54218898aad13bf24a0ebf07dcb9dc1428086297
JonathanBeltran <jbelt021@fiu.edu>
2016-07-08 15:08:36 -0400

    feat: adding services

---
+++ app/services.js
@@ -0,0 +1,13 @@
+angular.module("myApp.services", []).factory("resumeService", function() {
+  var STORAGE_ID = 'myApp.resumes', factory = { };
+
+factory.get = function() {
+  return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+};
+
+factory.put = function(songs) {
+  localStorage.setItem(STORAGE_ID, JSON.stringify(resumes));
+};
+
+return factory;
+});
