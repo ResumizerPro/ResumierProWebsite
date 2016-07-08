29cd7e9d2c4566f7d9de13a1927c0a327575f1b0
JonathanBeltran <jbelt021@fiu.edu>
2016-07-07 21:20:24 -0400

    feat: adding form control

--- app/controllers.js
+++ app/controllers.js
@@ -10,4 +10,15 @@ angular.module("myApp.controllers", []).controller("resumeCtrl", function($scope
   artist: "Within Temptation",
   title: "Ice Queen"
 }];
+
+$scope.newSong = {};
+
+$scope.addSong = function(/** String */ artist, /** String */ title) {
+  $scope.songs.push({
+    artist: artist,
+    title: title
+  });
+  $scope.newSong.title = "";
+  $scope.newSong.artist = "";
+};
 });
