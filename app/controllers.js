4c90c4f4e700ac363f57edd7b36c171df7cb4b80
JonathanBeltran <jbelt021@fiu.edu>
2016-07-09 12:13:02 -0400

    bug: saving session data

--- app/controllers.js
+++ app/controllers.js
@@ -18,6 +18,7 @@ angular.module("myApp.controllers", []).controller("resumeCtrl", function ($scop
     };
 
   $scope.$watch('resumes', function (newValue, oldValue) {
+    console.log('IM WATCHING');
     if (newValue !== oldValue) {
     resumeService.put($scope.resumes);
     $scope.resumes = resumeService.get();
