8e6f99575208fa999fe1df58ce4756414d0cfee2
Darkness8642 <Dominick.Martelly@gmail.com>
2016-07-09 17:36:32 -0400

    Change layout to better organize the site

--- server/routes/user.js
+++ server/routes/user.js
@@ -28,4 +28,38 @@ module.exports = function (app) {
             failureFlash: true
         }));
     app.get('/signout', users.signout);
+
+    //Registration
+    app.get('/register', function (req, res) {
+        res.render('register', {});
+    });
+
+    app.post('/register', function (req, res) {
+        User.register(new User({username: req.body.username}), req.body.password, function (err, account) {
+            if (err) {
+                return res.render('register', {account: account});
+            }
+
+            passport.authenticate('local')(req, res, function () {
+                res.redirect('/');
+            });
+        });
+    });
+
+    app.get('/login', function (req, res) {
+        res.send('login', {user: req.user});
+    });
+
+    app.post('/login', passport.authenticate('local'), function (req, res) {
+        res.redirect('/');
+    });
+
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.get('/ping', function (req, res) {
+        res.status(200).send("pong!");
+    });
 };
