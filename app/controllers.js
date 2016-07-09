75ff8fbf5fbccde154502cac256085812b3119c0
JonathanBeltran <jbelt021@fiu.edu>
2016-07-09 13:11:00 -0400

    feat: not allowing duplicates

--- app/controllers.js
+++ app/controllers.js
@@ -23,12 +23,14 @@ angular.module("myApp.controllers", []).controller("resumeCtrl", function ($scop
 
       $scope.resumes.map(function(a){
         var dup = true;
+        var count = 0;
         $scope.resumes.map(function(b){
           if(a == b){
-            dup =  false;
+            count++;
           }
         });
-        console.log(dup);
+        if(count > 1)
+          console.log(false);
       });
     }
 }, true);
