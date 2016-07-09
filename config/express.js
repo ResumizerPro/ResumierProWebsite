8e648624b568619a67ec57768efabea97bf4d8a9
JonathanBeltran <jbelt021@fiu.edu>
2016-07-09 15:43:05 -0400

    feat: navbar

--- config/express.js
+++ config/express.js
@@ -20,8 +20,7 @@ module.exports = function (app) {
     app.use('/scripts_1/', express.static(__dirname + "/../node_modules/bootstrap/dist/css"));
     app.use('/scripts_2/', express.static(__dirname + "/../node_modules/font-awesome/css"));
     app.use('/scripts_3/', express.static(__dirname + "/../node_modules/angular"));
-    app.use('/scripts_4/', express.static(__dirname + "/../node_modules/lodash"));
-    app.use('/scripts_5/', express.static(__dirname + "/../node_modules/underscore.string/dist"));
+    app.use('/scripts_4/', express.static(__dirname + "/../node_modules/bootstrap/dist/js"));
     app.use('/fonts', express.static(__dirname + "/../node_modules/font-awesome/fonts");
     app.use('/', express.static(__dirname + "/../client"));
     app.use('/app', express.static(__dirname + "/../app"));
