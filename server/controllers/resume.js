fe8797f3cd941e11003096f0136ee27191a6978d
JonathanBeltran <jbelt021@fiu.edu>
2016-07-11 01:44:55 -0400

    bug: changed find_one

--- server/controllers/resume.js
+++ server/controllers/resume.js
@@ -24,7 +24,7 @@ exports.findOneResume = function(req, res) {
 
 exports.addResume = function(req, res) {
     var document = new resume(req.body);
-    console.log(req.params.isPublic);
+    console.log(req.params("isPublic"));
 
     document.save(function(err, resume) {
         if (err) {
