71482cbd748e37b73e2428d397d2eba4610a04b9
JonathanBeltran <jbelt021@fiu.edu>
2016-07-23 15:12:46 -0400

    test: angular route

--- client/app/emptySignup.js
+++ client/app/emptySignup.js
@@ -2,7 +2,7 @@ $( "#signup" ).submit(function( event ) {
   if($("#inputEmail").val() == '' || $("#inputPassword").val() == '' || $("#inputRePassword").val() == '' || $("#inputUsername").val() == '')
     event.preventDefault();
   else if($("#inputPassword").val() != $("#inputRePassword").val()){
-    $("#alert_message").html("<h1>The password and retyped password do not match</h1>");
+    $("#alert_message").html("<h3>The password and retyped password do not match</h3>");
     event.preventDefault();
   }
 });
