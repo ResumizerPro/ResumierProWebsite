b754a37a3fb73585e2c2f6b6756c0bbd124a1647
Dominick Martelly <dmart120@fiu.edu>
2016-07-24 17:24:28 -0400

    Added new test cases

--- server/testing/testDriver.js
+++ server/testing/testDriver.js
@@ -13,30 +13,25 @@ var dbURI = 'mongodb://localhost/testing',
 mongoose.connect(dbURI);
 
 describe('Testing User Controller Class Method Login', function () {
-    beforeEach(function (done) {
-
-        new user({
-            username: 'Apple',
-            password: '1234',
-            email: '123abc@gmail.com',
-            provider: 'a'
-        }).save(done);
-    });
     describe('Test_Login_1', function () {
         it('Sunny-day where BobbyCreak is already sighed in', function (done) {
 
-            var expected = 'render:success.Welcome BobbyCreak';
+            var expected = 'render:success_message:Welcome BobbyCreak';
 
             var output = '';
             var req = {
                 user: 'BobbyCreak',
                 flash: function (string) {
-                    output = output + 'flash:' + string + '.';
-                }
+                    output = output + 'flash:' + string + '_';
+                },
+                logout: function() {}
             };
             var res = {
                 render: function (string, msg) {
-                    output = output + 'render:' + string + '.' + msg.messages;
+                    output = output + 'render:' + string + '_' + 'message:' + msg.messages;
+                },
+                redirect: function (string) {
+                    output = output + 'redirect:' + string;
                 }
             };
             userController.login(req, res);
@@ -45,20 +40,24 @@ describe('Testing User Controller Class Method Login', function () {
         });
     });
     describe('Test_Login_2', function () {
-        it('Sunny-day where MJ is already sighed in', function (done) {
+        it('Sunny-day where \'!@#$^^&*\' is already sighed in', function (done) {
 
-            var expected = 'render:success.Welcome MJ';
+            var expected = 'render:success_message:Welcome !@#$^^&*';
 
             var output = '';
             var req = {
-                user: 'MJ',
+                user: '!@#$^^&*',
                 flash: function (string) {
-                    output = output + 'flash:' + string + '.';
-                }
+                    output = output + 'flash:' + string + '_';
+                },
+                logout: function() {}
             };
             var res = {
                 render: function (string, msg) {
-                    output = output + 'render:' + string + '.' + msg.messages;
+                    output = output + 'render:' + string + '_' + 'message:' + msg.messages;
+                },
+                redirect: function (string) {
+                    output = output + 'redirect:' + string;
                 }
             };
             userController.login(req, res);
@@ -67,20 +66,198 @@ describe('Testing User Controller Class Method Login', function () {
         });
     });
     describe('Test_Login_3', function () {
-        it('Rainy-day where No one is signed in', function (done) {
+        it('Rainy-day where \'\' (A blank string) is signed in', function (done) {
 
-            var expected = 'flash:error_flash:info_render:login_undefined';
+            var expected = 'flash:error_flash:info_render:login_message:undefined';
+
+            var output = '';
+            var req = {
+                user: '',
+                flash: function (string) {
+                    output = output + 'flash:' + string + '_';
+                },
+                logout: function() {}
+            };
+            var res = {
+                render: function (string, msg) {
+                    output = output + 'render:' + string + '_' + 'message:' + msg.messages;
+                },
+                redirect: function (string) {
+                    output = output + 'redirect:' + string;
+                }
+            };
+            userController.login(req, res);
+            assert.equal(output, expected);
+            done();
+        });
+    });
+});
+describe('Testing User Controller Class Method Success', function () {
+    describe('Test_Success_1', function () {
+        it('Sunny-day. Only one case since the method does not take any outside parameters', function (done) {
+
+            var expected = 'render:success_message:Please exit this menu.';
 
             var output = '';
             var req = {
                 user: undefined,
                 flash: function (string) {
                     output = output + 'flash:' + string + '_';
-                }
+                },
+                logout: function() {}
             };
             var res = {
                 render: function (string, msg) {
-                    output = output + 'render:' + string + '_' + msg.messages;
+                    output = output + 'render:' + string + '_' + 'message:' + msg.messages;
+                },
+                redirect: function (string) {
+                    output = output + 'redirect:' + string;
+                }
+            };
+            userController.success(req, res);
+            assert.equal(output, expected);
+            done();
+        });
+    });
+});
+describe('Testing User Controller Class Method Signup_render', function () {
+    describe('Test_Success_1', function () {
+        it('Sunny-day. Only one case since the method does not take any outside parameters', function (done) {
+
+            var expected = 'render:signup_message:';
+
+            var output = '';
+            var req = {
+                user: undefined,
+                flash: function (string) {
+                    output = output + 'flash:' + string + '_';
+                },
+                logout: function() {}
+            };
+            var res = {
+                render: function (string, msg) {
+                    output = output + 'render:' + string + '_' + 'message:' + msg.messages;
+                },
+                redirect: function (string) {
+                    output = output + 'redirect:' + string;
+                }
+            };
+            userController.signup_render(req, res);
+            assert.equal(output, expected);
+            done();
+        });
+    });
+});
+describe('Testing User Controller Class Method signout', function () {
+    describe('Test_Signout_1', function () {
+        it('Sunny-day. Only one case since the method does not take any outside parameters', function (done) {
+
+            var expected = 'redirect:/';
+
+            var output = '';
+            var req = {
+                user: undefined,
+                flash: function (string) {
+                    output = output + 'flash:' + string + '_';
+                },
+                logout: function() {}
+            };
+            var res = {
+                render: function (string, msg) {
+                    output = output + 'render:' + string + '_' + 'message:' + msg.messages;
+                },
+                redirect: function (string) {
+                    output = output + 'redirect:' + string;
+                }
+            };
+            userController.signout(req, res);
+            assert.equal(output, expected);
+            done();
+        });
+    });
+});
+describe('Testing User Controller Class Method Login', function () {
+    beforeEach(function (done) {
+        new user({
+            username: 'Apple',
+            password: '1234',
+            email: '123abc@gmail.com',
+            provider: 'a'
+        }).save(done);
+    });
+    describe('Test_Signup_1', function () {
+        it('Sunny-day where a new user creates an account', function (done) {
+
+            var expected = 'render:success_message:You are logged in. Please exit this menu.';
+
+            var output = '';
+            var req = {
+                user: undefined,
+                flash: function (string) {
+                    output = output + 'flash:' + string + '_';
+                },
+                logout: function() {}
+            };
+            var res = {
+                render: function (string, msg) {
+                    output = output + 'render:' + string + '_'
+                        + 'title:' + msg.title + '_'
+                        + 'message:' + msg.messages;
+                },
+                redirect: function (string) {
+                    output = output + 'redirect:' + string;
+                }
+            };
+            userController.login(req, res);
+            assert.equal(output, expected);
+            done();
+        });
+    });
+    describe('Test_Signup_2', function () {
+        it('Sunny-day where a new user creates an account', function (done) {
+
+            var expected = 'render:success_message:Welcome !@#$^^&*';
+
+            var output = '';
+            var req = {
+                user: '!@#$^^&*',
+                flash: function (string) {
+                    output = output + 'flash:' + string + '_';
+                },
+                logout: function() {}
+            };
+            var res = {
+                render: function (string, msg) {
+                    output = output + 'render:' + string + '_' + 'message:' + msg.messages;
+                },
+                redirect: function (string) {
+                    output = output + 'redirect:' + string;
+                }
+            };
+            userController.login(req, res);
+            assert.equal(output, expected);
+            done();
+        });
+    });
+    describe('Test_Signup_3', function () {
+        it('Rainy-day where \'\' (A blank string) is signed in', function (done) {
+
+            var expected = 'flash:error_flash:info_render:login_message:undefined';
+
+            var output = '';
+            var req = {
+                user: '',
+                flash: function (string) {
+                    output = output + 'flash:' + string + '_';
+                },
+                logout: function() {}
+            };
+            var res = {
+                render: function (string, msg) {
+                    output = output + 'render:' + string + '_' + 'message:' + msg.messages;
+                },
+                redirect: function (string) {
+                    output = output + 'redirect:' + string;
                 }
             };
             userController.login(req, res);
