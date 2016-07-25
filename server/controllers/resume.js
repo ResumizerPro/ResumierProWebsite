f29c51cfa16bc5a5f750e80a68198832a85ff8bd
JonathanBeltran <jbelt021@fiu.edu>
2016-07-24 22:49:26 -0400

    feat: create template

--- server/controllers/resume.js
+++ server/controllers/resume.js
@@ -4,6 +4,59 @@ var user = mongoose.model('user');
 
 var path = require('path');
 
+var Pages = [
+  'contact_info',
+  'education',
+  'Skills',
+  'Projects',
+  'work_experence'
+]
+var ContactInfo = [
+  'full_name',
+  'email',
+  'phone_number',
+  'gitHub',
+  'linkedIn'
+]
+var Education = [
+  'university',
+  'start_Month',
+  'end_Month',
+  'start_Year',
+  'end_Year',
+  'degree',
+  'city'
+]
+var Skills = [
+  'programming_languages',
+  'iDEs',
+  'technologies',
+  'databases'
+]
+var Projects = [
+  'project_name',
+  'project_link',
+  'project_description',
+  'project_implementation',
+  'project_technologies'
+]
+var WorkExperience = [
+  'job_title',
+  'employer',
+  'city',
+  'start_month',
+  'start_year',
+  'end_month',
+  'end_year',
+  'job_description',
+]
+
+
+
+
+
+
+
 //Trying to chang to search resumes
 exports.findAllResumes = function (req, res) {
     resume.find({
@@ -48,82 +101,54 @@ exports.addResume = function (req, res) {
 
                   var exec = require('child_process').exec;
                   var cmd = 'sudo ./../../templates/laton text.tex input.sty helvetica.sty res.cls';
-                  var Pages = [
-                    'contact_info',
-                    'education',
-                    'Skills',
-                    'Projects',
-                    'work_experence'
-                  ]
-                  var ContactInfo = [
-                    'full_name',
-                    'email',
-                    'phone_number',
-                    'gitHub',
-                    'linkedIn'
-                  ]
-                  var Education = [
-                    'university',
-                    'start_Month',
-                    'end_Month',
-                    'start_Year',
-                    'end_Year',
-                    'degree',
-                    'city'
-                  ]
-                  var Skills = [
-                    'programming_Languages',
-                    'iDEs',
-                    'technologies',
-                    'databases'
-                  ]
-                  var Projects = [
-                    'project_name',
-                    'project_link',
-                    'project_description',
-                    'project_implementation',
-                    'project_technologies'
-                  ]
-                  var WorkExperience = [
-                    'job_title',
-                    'employer',
-                    'city',
-                    'start_month',
-                    'start_year',
-                    'end_month',
-                    'end_year',
-                    'job_description',
-                  ]
-                  console.log(Object.keys(resume));
-                  for (var x in Pages){
-                    switch(Pages[x]){
-                      case 'contact_info':
-                        for (var y in ContactInfo){
-                          console.log(ContactInfo[y] + ': ' + resume[Pages[x]][0][ContactInfo[y]]);
+                  var fs = require('fs');
+                  var latex;
+                  var cmmd = "\\newcommand";
+                    for (var x in Pages){
+                      switch(Pages[x]){
+                        case 'contact_info':
+                          for (var y in ContactInfo){
+                            var name = '\\' + ContactInfo[y];
+                            latex += cmmd + "{" + name + "}" + "{"+ resume[Pages[x]][0][ContactInfo[y]] + "}" ;
+                            console.log(ContactInfo[y] + ': ' + resume[Pages[x]][0][ContactInfo[y]]);
+                          }
+                          break;
+                        case 'education':
+                          for (var y in Education){
+                            var name = '\\' + Education[y];
+                            latex += cmmd + "{" + name + "}" + "{"+ resume[Pages[x]][0][Education[y]] + "}" ;
+                            console.log(Education[y] + ': ' + resume[Pages[x]][0][Education[y]]);
+                          }
+                          break;
+                        case 'Skills':
+                          for (var y in Skills){
+                            var name = '\\' + Skills[y];
+                            latex += cmmd + "{" + name + "}" + "{"+ resume[Pages[x]][0][Skills[y]] + "}" ;
+                            console.log(Skills[y] + ': ' + resume[Pages[x]][0][Skills[y]]);
+                          }
+                          break;
+                        case 'Projects':
+                          for (var y in Projects){
+                            var name = '\\' + Projects[y];
+                            latex += cmmd + "{" + name + "}" + "{"+ resume[Pages[x]][0][Projects[y]] + "}" ;
+                            console.log(Projects[y] + ': ' + resume[Pages[x]][0][Projects[y]]);
+                          }
+                          break;
+                        case 'work_experence':
+                          for (var y in WorkExperience){
+                            var name = '\\' + WorkExperience[y];
+                            latex += cmmd + "{" + name + "}" + "{"+ resume[Pages[x]][0][WorkExperience[y]] + "}" ;
+                            console.log(WorkExperience[y] + ': ' + resume[Pages[x]][0][WorkExperience[y]]);
+                          }
+                          break;
                         }
-                      break;
-                      case 'education':
-                        for (var y in Education){
-                          console.log(Education[y] + ': ' + resume[Pages[x]][0][Education[y]]);
-                        }
-                      break;
-                      case 'Skills':
-                        for (var y in Skills){
-                          console.log(Skills[y] + ': ' + resume[Pages[x]][0][Skills[y]]);
-                        }
-                      break;
-                      case 'Projects':
-                        for (var y in Projects){
-                          console.log(Projects[y] + ': ' + resume[Pages[x]][0][Projects[y]]);
-                        }
-                      break;
-                      case 'work_experence':
-                        for (var y in WorkExperience){
-                          console.log(WorkExperience[y] + ': ' + resume[Pages[x]][0][WorkExperience[y]]);
-                        }
-                      break;
-                    }
                   }
+                  fs.writeFile("/../../templates/input_1.sty",latex , function(err) {
+                    if(err) {
+                      return console.log(err);
+                    }
+                    console.log("The file was saved!");
+                  });
                   /*var fs = require('fs');
                   fs.writeFile("/../../templates/input_1.sty",
                     var comd = "\newcommand";
