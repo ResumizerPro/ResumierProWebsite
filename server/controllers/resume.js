d2a35b7cd28dcdec55608de3b5c7245f4ea134d4
JonathanBeltran <jbelt021@fiu.edu>
2016-07-24 23:30:08 -0400

    feat: resume generation

--- server/controllers/resume.js
+++ server/controllers/resume.js
@@ -100,7 +100,7 @@ exports.addResume = function (req, res) {
                 } else {
 
                   var exec = require('child_process').exec;
-                  var cmd = 'sudo ' + __dirname + './../../templates/laton text.tex input_1.sty helvetica.sty res.cls';
+                  var cmd = 'sudo ./' + __dirname + '/../../templates/laton text.tex input_1.sty helvetica.sty res.cls';
                   var fs = require('fs');
                   var latex = '';
                   var cmmd = "\\newcommand";
@@ -161,7 +161,7 @@ exports.addResume = function (req, res) {
                   exec(cmd, function(error, stdout, stderr) {
                     console.log(stderr);
                   });
-                  cmd = "mv -f" + __dirname + "/text.pdf " + __dirname + "/../../templates/";
+                  cmd = "mv " + __dirname + "/text.pdf " + __dirname + "/../../templates/";
                   exec(cmd, function(error, stdout, stderr) {
                     console.log(stderr);
                   });
